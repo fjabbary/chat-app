5b40a8a7fe0f46cc54a672774e4878f57b11e4db
Farzin Jabbary <farzin.jabbary@gmail.com>
2025-05-26 12:53:55 -0700

    backend api done

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import dotenv from 'dotenv';
+import connectDB from './lib/db.js';
+import cookieParse from "cookie-parser";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cookieParse());
+
+app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB()
+}
+);
