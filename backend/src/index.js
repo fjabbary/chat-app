330df4b617d08c12d566c4536e50f132adcbabce
Farzin Jabbary <farzin.jabbary@gmail.com>
2025-05-26 20:43:14 -0700

    react app basic setup done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
 import connectDB from './lib/db.js';
 import cookieParse from "cookie-parser";
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,11 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParse());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}
+))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
